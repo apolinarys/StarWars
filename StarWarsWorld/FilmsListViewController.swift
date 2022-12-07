20f9ac5d393a0107e95646ee1e2dc84d90460392
Polina <112948671+apolinarys@users.noreply.github.com>
2022-12-07 13:35:19 +0300

    Создан экран списка фильмов

---
+++ StarWarsWorld/FilmsListViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StarWarsWorld
+//
+//  Created by Macbook on 07.12.2022.
+//
+
+import UIKit
+
+class FilmsListViewController: UIViewController {
+
+    @IBOutlet weak var filmsListTableView: UITableView!
+    @IBOutlet weak var searchBar: UISearchBar!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
